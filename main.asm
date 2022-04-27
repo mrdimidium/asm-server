6400b1285c5174c5a809f09db79a42575c0769b0
Nikolay Govorov <me@govorov.online>
2022-04-28 00:46:56 +0400

    Add the macro for sys_call

--- main.asm
+++ main.asm
@@ -1,3 +1,8 @@
+%macro sys_call 1
+	mov rax, %1
+	syscall
+%endmacro
+
 	section .data
 
 msg:	db "HTTP/1.1 200 OK",13,10,"content-length: 28",13,10,"content-type: text/html",13,10,10,"<h1>Hello, NASM server!</h1>"
@@ -6,11 +11,10 @@ msg_l:	equ $-msg
 	section .text
 	global _start
 
-_start: mov rax, 41	; create socket (SYS_SOCKET, opcode 41)
-	mov rdi, 2	; family   = PF_INET
+_start: mov rdi, 2	; family   = PF_INET
 	mov rsi, 1	; type     = SOCK_STREAM
 	mov rdx, 6	; protocol = IPPROTO_TCP
-	syscall
+	sys_call 41	; create socket (SYS_SOCKET, opcode 41)
 
 	; bind
 	push qword 0	; end struct on stack (arguments get pushed in reverse order)
@@ -19,13 +23,11 @@ _start: mov rax, 41	; create socket (SYS_SOCKET, opcode 41)
 	mov rdi, rax	; save listen descriptor
 	mov rsi, rsp	; save sokaddr ptr
 	mov rdx, 0x10	; save addrlen
-	mov rax, 49	; bind socket (SYS_BIND, opcode 49) 
-	syscall
+	sys_call 49	; bind socket (SYS_BIND, opcode 49) 
 
 	; listen
 	mov rsi, 64	; backlog=64
-	mov rax, 50	; listen socket (SYS_LISTEN, opcode 50)
-	syscall
+	sys_call 50	; listen socket (SYS_LISTEN, opcode 50)
 
 	jmp _accept
 
@@ -35,8 +37,7 @@ _clr:	push qword rdi
 	mov rsi, 0
 	mov rdx, 1	; =WNOHANG
 	mov r10, 0
-	mov rax, 61	; wait any children (SYS_WAIT4)
-	syscall
+	sys_call 61	; wait any children (SYS_WAIT4)
 	pop qword rdi
 	test rax, rax
 	jnz _clr
@@ -45,13 +46,11 @@ _accept:
 	; accept new connection
 	mov rsi, 0
 	mov rdx, 0
-	mov rax, 43	; accept socket (SYS_ACCEPT, opcode 43)
-	syscall
+	sys_call 43	; accept socket (SYS_ACCEPT, opcode 43)
 
 	; fork
 	mov rsi, rax	; move return value of SYS_SOCKET into esi (file descriptor for accepted socket, or -1 on error)
-	mov rax, 57	; create a new process by request (SYS_FORK, opcode 2)
-	syscall
+	sys_call 57	; create a new process by request (SYS_FORK, opcode 2)
 
 	test rax, rax	; if return value of SYS_FORK in eax is zero we are in the child process
 	jnz _clr	; jmp on new listen iterration if it not child process
@@ -60,16 +59,12 @@ _accept:
 	mov rdi, rsi	; move accepted file descriptor
 	mov rsi, msg	; move reponse address
 	mov rdx, msg_l	; move response size 
-	mov rax, 1	; write on socket (SYS_WRITE)
-	syscall
-	mov rax, 3	; close listen descriptor (SYS_CLOSE)
+	sys_call 1	; write on socket (SYS_WRITE)
 	mov rdi, rdi
-	syscall
-	mov rax, 3	; close accept descriptor (SYS_CLOSE)
+	sys_call 3	; close listen descriptor (SYS_CLOSE)
 	mov rdi, rsi
-	syscall
+	sys_call 3	; close accept descriptor (SYS_CLOSE)
 
 _exit:	mov rdi, 0	; 0 errors
-	mov rax, 60	; invoke SYS_EXIT
-	syscall
+	sys_call 60	; invoke SYS_EXIT
 
